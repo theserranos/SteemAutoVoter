7999041088a844c8232b347a2f3856532f849046
Devba <Devba@users.noreply.github.com>
2017-04-04 15:27:44 +0200

    Fixing vote() function issue
    Also debugging the code

--- functions.js
+++ functions.js
@@ -8,8 +8,7 @@ function CleanNav(){
 	$("#votbot").hide();
 	$("#accinfo").hide();
 	$("#inicio").hide();
-	
-	
+		
 }
 
 
@@ -34,21 +33,22 @@ function showAccountInfo(){
       $("#cuerpo").html(aux);
        console.log('--------inside ShowAccountInfo');
   })
-console.log('------getting account');
+console.log('------getting account ');
 
 }
 
 
 
 
-function vote(){
-  console.log('voting');
+function vote(vvote){
+	//voting for last nomadsteem post'
 
-  var voter="nomadsteem";
-  var username="nomadsteem";
-  var password=$("#PostKey").val();;
-  var author= $("#vauthor").val();//"fisteganos";
-  var permlink= r[0].permlink;
+  console.log('voting ');
+
+  var voter=localstorage.username;
+  var password=localStorage.PostingKey;
+  var author= vvote.author;
+  var permlink= vvote.permlink;
   var weight=100;
   
 
@@ -61,33 +61,34 @@ steem.broadcast.vote(password, voter, author, permlink, weight, function(err, re
  
 function CheckVoted(){
 //last post by nomadsteem
+      var vfound='false';
       steem.api.getDiscussionsByAuthorBeforeDate('nomadsteem', '', '2018-03-20T20:27:30', 1, function(err, r) {
       console.log('---- last '+ err, r);
+      //vfound ='false';
 
-
-for (var key in r[0].active_votes){
+       for (var key in r[0].active_votes){
        	  
-       	   if (r[0].active_votes[key].voter=='steemalf'){
-           vfound='true';
-       	   }
+       	   if (r[0].active_votes[key].voter=='nomadsteem'){
+             vfound='true' ;
+      	   }
        }
 
   
   if (vfound!='true'){
-  	vote();
-  }
+  	vote(r[0]);
+  	// discussions();
+  } else {
     discussions();
+  }
   });
 
-	
+//return	
 }
 
 
 
-
 function discussions(){
-
-    
+//Randomly selects one post  
 
     var categ = $("#Tagfilter").val().split(",");
 	var vnumcateg=categ.length;
@@ -99,99 +100,88 @@ function discussions(){
 	steem.api.getDiscussionsByCreated(query, function(err, result) {
 		var rndpost=Math.floor((Math.random() * 30) + 1);
 		console.log('--- post selected :' + rndpost);
-		votediscussion(result[rndpost]);
+		var res=result[rndpost]
+		votediscussion(res);
+		return;
 	});
 }
 
 
 
-function votediscussion(r){
+function votediscussion(vpostselected){
 	 console.log('----voting');
 	 
+   	localStorage.setItem("voter", $("#bvoter").val());
+	localStorage.setItem("username", $("#busername").val());
+	localStorage.setItem("PostingKey", $("#PostKey").val());
+	localStorage.setItem("Tags", $("#Tagfilter").val());
+	localStorage.setItem("VotingPower", $("#VoteP").val());
+	localStorage.setItem("Reftime", $("#RefreshTime").val());
 
 
-  var voter=$("#bvoter").val();
-  var username=$("#busername").val();
-  var password=$("#PostKey").val();//"";
-
-localStorage.setItem("voter", $("#bvoter").val());
-localStorage.setItem("username", $("#busername").val());
-localStorage.setItem("PostingKey", $("#PostKey").val());
-
-console.log(localStorage.voter )
-
-
-  if(!r){
+  if(!vpostselected){
   	console.log ('--noo hay post ');
   	var vfail=$("#votefail")[0].innerText;
-  	 $("#votefail")[0].innerText=parseInt(vfail)+1;
-  	 		var d = new Date(); var ahora=d.getHours()+': '+ d.getMinutes() + ': ' + d.getSeconds();
-  	  document.getElementById("bodytabla").insertRow(0).innerHTML = ("<td>" + ahora +'</td><td>' +'--'
-			+ '</td><td>'+ '----' +'</td><td>'+ '-----'+'</td><td  class="alert-danger"' +'">'+ 'error'+'</td><td >'+ 'error' +"</td>");
+  	$("#votefail")[0].innerText=parseInt(vfail)+1;
+  	var d = new Date(); var ahora=d.getHours()+': '+ d.getMinutes() + ': ' + d.getSeconds();
+  	document.getElementById("bodytabla").insertRow(0).innerHTML = ("<td>" + ahora +'</td><td>' +'--'
+			+ '</td><td>'+ 'Could not find a random post to vote in ' +'</td><td>'+ '-----'+'</td><td  class="alert-danger"' +'">'+ 'error'+'</td><td >'+ 'error' +"</td>");
   	return;
   }
 
-  var author= r.author ;//$("#vvoteauthor").val();//"fisteganos";
-  var permlink=r.permlink;// $("#vvoteperm").val();
-  var weight= $("#VoteP").val()*100 ; //+Math.floor((Math.random() * 100) + 1);
-  
 
- 	wif=password;
-	var vres=null;
+    var weight= $("#VoteP").val()*100 ; 
+    var vres=null;
 	var voteresp=null;
+	var vp=0;
 	
-steem.broadcast.vote(wif, voter, author, permlink, weight, function(err, voteresp) {
+steem.broadcast.vote(localStorage.PostingKey, localStorage.username, vpostselected.author, vpostselected.permlink, weight, function(err, voteresp) {
 	if (err){
-			 vres='-- Error : --',err.message;
-			console.log('----------',vres);
-			var vfail=$("#votefail")[0].innerText;
-			   $("#votefail")[0].innerText=parseInt(vfail)+1;
+			vres='-- Error : --'+err.cause.payload.error.data.message;
+			console.log('----------'+vres);
+			var vfail=$("#votefail")[0].innerText;$("#votefail")[0].innerText=parseInt(vfail)+1;
 				vcolor='alert-danger'
+						var d = new Date(); var ahora=d.getHours()+': '+ d.getMinutes() + ': ' + d.getSeconds()
+			 document.getElementById("bodytabla").insertRow(0).innerHTML = ("<td>" + d +'</td><td>' +vpostselected.author 
+					+ '</td><td>'+ $("#VoteP").val() +'</td><td>'+ vpostselected.permlink+'</td><td  class="alert ' + vcolor +'">'+ vres+'</td><td >'+ vp +"</td>");
+				return
 		}
 	else
 		{
+
 			console.log('block',voteresp.ref_block_num);
-		   vres='bloque : ' + voteresp.ref_block_num;
+		    vres='bloque : ' + voteresp.ref_block_num;
 			vcolor='alert-success'
-		}
+			var d = new Date();
+			
+		
 	
-		steem.api.getAccounts([voter], function(err, response){
-			console.log('---Accinfo response');
+		   steem.api.getAccounts([localStorage.username], function(err, response){
+		     	console.log('---Accinfo response');
 				AccInfo=response[0];
-		});
-       
-        var vp='Not Available';
-        if(AccInfo){
-        var old_votingPw=AccInfo.voting_power/100;  
-        vp=	AccInfo.voting_power/100;
-        }        
-        var valert="<div class='alert " + vcolor +"'>" + vp + "</div>";
-		var d = new Date(); var ahora=d.getHours()+': '+ d.getMinutes() + ': ' + d.getSeconds()
-	 document.getElementById("bodytabla").insertRow(0).innerHTML = ("<td>" + ahora +'</td><td>' +r.author 
-			+ '</td><td>'+ $("#VoteP").val() +'</td><td>'+ r.permlink+'</td><td  class="alert ' + vcolor +'">'+ vres+'</td><td >'+ vp +"</td>");
 
-
+				var vp='Not Available';
+				if(AccInfo){
+				var old_votingPw=AccInfo.voting_power/100;  
+				vp=	AccInfo.voting_power/100;
+				}    
+			document.getElementById("bodytabla").insertRow(0).innerHTML = ("<td>" + d +'</td><td>' +vpostselected.author 
+					+ '</td><td>'+ $("#VoteP").val() +'</td><td>'+ vpostselected.permlink+'</td><td  class="alert ' + vcolor +'">'+ vres+'</td><td >'+ vp +"</td>");
+				    
 			var vsuccess=$("#votesuccess")[0].innerText;
-
-	
-	$("#votesuccess")[0].innerText=parseInt(vsuccess)+1;
+			$("#votesuccess")[0].innerText=parseInt(vsuccess)+1;
+		});
+       }
+        
 });
 
 }
 
  	
-	
-	
-
-
-
 function regvote(){
 	CheckVoted();
 setTimeout(function(){ regvote(); }, $("#RefreshTime").val()*1000);
 }
-
-
-
 
 
 function subscribe(){
@@ -203,17 +193,14 @@ var e,r = null;
 
 var devuelta=function (r){
 	alert('óoooo');
-console.log('----cbaaakkk',e,r);}
+console.log('----cbaaakkk',e,result);}
 
 
 function callback (e,result){
 	alert('ppp');
 console.log('----cbaaakkk222',e,result);}
 
-steem.api.setSubscribeCallback('1', 'false', function(err, result) {
-  console.log(err, result);
-  callback(err,result);
-});
+var x=steem.api.setSubscribeCallback('1', '',devuelta);
 ;
 
 
@@ -258,22 +245,4 @@ function Feed(){
 steem.api.getFeedHistory(function(err, result) {
   console.log(err, result);
 });
-}
-
-
-function desayuno(food,drink,callback){
-	console.log('desayuno '+food + " y "+ drink);
-	if(callback && typeof(callback)==="function"){
-		callback()
-	}
-}
-
-function quedes(){
-	desayuno('cafe','pan',function(){
-	console.log('ya termine de desayunar')});
-	if (typeof(Storage) !== "undefined") {
-    console.log(' Code for localStorage/sessionStorage.');
-} else {
-    // Sorry! No Web Storage support..
-}
 }
